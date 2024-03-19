97f96476296b85150150cf729f4abb9327535c04
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-03-19 12:43:45 +0530

    initial commit

---
+++ FastAutomate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastAutomate
+//
+//  Created by Tissa Macbook on 19/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
